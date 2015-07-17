43c1a2fdb687c1ec41f702070a73d8461a2984c7
hiro <hiro0218@gmail.com>
2015-07-17 15:50:20 +0900

    fix
    デグレ

--- app/views/footer.php
+++ app/views/footer.php
@@ -16,8 +16,17 @@
 			<div class="col-xs-12">
 				<footer>
 					<p>Produced by <a href="https://twitter.com/<?= $init->twitterID ?>"><?= $init->adminName ?></a>
-                         (<a href="<?= $init->urlTopPage ?>"><?= $init->urlTopPage ?></a>)
-                        </p>
+						 (<a href="<?= $init->urlTopPage ?>"><?= $init->urlTopPage ?></a>)
+<?php if($init->performance) : ?>
+	<small class="pull-right">
+	<?php
+	list($tmp1, $tmp2) = array_pad( explode(" ", $init->CPU_start), 2, 0);
+	list($tmp3, $tmp4) = array_pad( explode(" ", microtime()), 2, 0);
+	printf("(CPU: %.6f秒)", $tmp4-$tmp2+$tmp3-$tmp1);
+	?>
+	</small>
+<?php endif; ?>
+					</p>
 					</footer>
 				</div>
 			</div>
