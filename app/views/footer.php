61f14938d1fb9c46a0a41cbcf288015f84fadcbc
hiro0218 <hiro0218@gmail.com>
2020-02-24 21:02:46 +0900

    fix: クレジットをフッターへ移動

--- app/views/footer.php
+++ app/views/footer.php
@@ -1,13 +1,14 @@
-<hr>
-		<script>
-			document.addEventListener("DOMContentLoaded", init(), false);
-		</script>
+			<hr>
 
-		<div class="row">
-			<div class="col-xs-12">
-				<footer>
-					<p>Produced by <a href="https://twitter.com/<?= $init->twitterID ?>"><?= $init->adminName ?></a>
-						 (<a href="<?= $init->urlTopPage ?>"><?= $init->urlTopPage ?></a>)
+			<script>
+				document.addEventListener("DOMContentLoaded", init(), false);
+			</script>
+
+			<div class="row">
+				<div class="col-xs-12">
+					<footer>
+						<p>Produced by <a href="https://twitter.com/<?= $init->twitterID ?>" target="_blank"><?= $init->adminName ?></a>
+						 (<a href="<?= $init->urlTopPage ?>" target="_blank"><?= $init->urlTopPage ?></a>)
 <?php if($init->performance) : ?>
 	<small class="pull-right">
 	<?php
@@ -17,7 +18,17 @@
 	?>
 	</small>
 <?php endif; ?>
-					</p>
+						</p>
+
+						<ul class="list-inline">
+							<li><a href="http://www.bekkoame.ne.jp/~tokuoka/hakoniwa.html" target="_blank">箱庭諸島スクリプト配布元</a> <a href="http://scrlab.g-7.ne.jp">[PHP]</a></li>
+							<li><a href="http://hakoniwa.symphonic-net.com" target="_blank">箱庭諸島S.E配布元</a></li>
+							<li><a href="http://snufkin.jp.land.to" target="_blank">沙布巾の箱庭</a></li>
+							<li><a href="http://www.s90259900.onlinehome.us/" target="_blank">箱庭の箱庭</a></li>
+							<li><a href="http://no-one.s53.xrea.com" target="_blank">The Return of Neptune</a></li>
+							<li><a href="http://minnano.min-ai.net/ocn/" target="_blank">みんなのあいらんど</a></li>
+							<li><a href="https://github.com/hiro0218/hakoniwa/" target="_blank">hakoniwa</a></li>
+						</ul>
 					</footer>
 				</div>
 			</div>
