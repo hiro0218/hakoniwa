ee66637a9bd3e2d3c3c5a8bc5d31a7085b9309d5
hiro0218 <hiro0218@gmail.com>
2020-02-24 22:55:04 +0900

    update: bootstrap v3.3.5 -> v3.3.7

--- app/views/template.php
+++ app/views/template.php
@@ -4,7 +4,7 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title><?= $init->title ?></title>
-	<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 	<link rel="stylesheet" type="text/css" href="<?= $init->cssDir ?>/<?= $init->css ?>">
 	<link rel="shortcut icon" href="<?= $init->imgDir ?>/monster1.gif">
 	<script type="text/javascript" src="<?= $init->jsDir ?>/<?= $init->js ?>"></script>
