dbde4cbf8b7642652b1844d6d6ddba37a53c70c6
hiro0218 <hiro0218@gmail.com>
2020-02-25 23:19:55 +0900

    fix: BFが存在している場合のみ表示

--- app/views/top/bf-list.php
+++ app/views/top/bf-list.php
@@ -1,7 +1,10 @@
 <?php
-echo "<div ID=\"IslandView\">\n";
-echo "<h2>Battle Fieldの状況</h2>\n";
+// Battle Fieldに設定された島がある場合のみ表示
+if ($hako->islandNumberNoBF < $hako->islandNumber) {
+    echo "<div ID=\"IslandView\">\n";
+    echo "<h2>Battle Fieldの状況</h2>\n";
 
-$this->islandTable($hako, $hako->islandNumberNoBF, $hako->islandNumber);
+    $this->islandTable($hako, $hako->islandNumberNoBF, $hako->islandNumber);
 
-echo "<hr>\n";
+    echo "<hr>\n";
+}
