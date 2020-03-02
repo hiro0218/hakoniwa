86b94ce19b18873eaf3c676564dc0c680ac179e8
hiro0218 <hiro0218@gmail.com>
2020-03-02 17:28:56 +0900

    refactor: Viewを分離

--- app/presenter/HtmlMente.php
+++ app/presenter/HtmlMente.php
@@ -8,6 +8,7 @@ class HtmlMente extends HTML {
         global $init;
 
 		$this->this_file = $init->baseDir. "/hako-mente.php";
+        $this->exist_log = is_dir("{$init->dirName}");
     }
 
 	function enter() {
@@ -19,82 +20,34 @@ class HtmlMente extends HTML {
 	function main($data) {
 		global $init;
 
-		require_once(VIEWS_PATH.'/admin/mente/main.php');
+        $this->initData($data);
 
-        // バックアップデータ
-        $this->backupPrint($data);
+        require_once(VIEWS_PATH.'/admin/mente/main.php');
 	}
 
 	// 表示モード
-	function dataPrint($data, $suf = "") {
+	function initData($data, $suf = "") {
 		global $init;
 
-		if(strcmp($suf, "") == 0) {
-			$fp = fopen("{$init->dirName}/hakojima.dat", "r");
-			echo "<h2>現役データ</h2>\n";
-		} else {
-			$fp = fopen("{$init->dirName}.bak{$suf}/hakojima.dat", "r");
-			echo "<h2>バックアップ{$suf}</h2>\n";
-        }
+        $this->is_backup = strcmp($suf, "") !== 0;
+        $file_dat = !$this->is_backup ? "{$init->dirName}/hakojima.dat" : "{$init->dirName}.bak{$suf}/hakojima.dat";
+        $this->readDatFile($file_dat);
+        $this->timeString = self::timeToString($this->lastTime);
 
-		$lastTurn = chop(fgets($fp, READ_LINE));
-		$lastTime = chop(fgets($fp, READ_LINE));
-		fclose($fp);
-		$timeString = self::timeToString($lastTime);
-		echo <<<END
-<strong>ターン$lastTurn</strong><br>
-<strong>最終更新時間</strong>:$timeString<br>
-<strong>最終更新時間(秒数表示)</strong>:1970年1月1日から$lastTime 秒<br>
-<form action="{$this->this_file}" method="post">
-<input type="hidden" name="PASSWORD" value="{$data['PASSWORD']}">
-<input type="hidden" name="mode" value="DELETE">
-<input type="hidden" name="NUMBER" value="{$suf}">
-<input type="submit" value="このデータを削除">
-</form>
-END;
-		if(strcmp($suf, "") == 0) {
-			$time = localtime($lastTime, TRUE);
-			$time['tm_year'] += 1900;
-			$time['tm_mon']++;
-			echo <<<END
-<h2>最終更新時間の変更</h2>
-<form action="{$this->this_file}" method="post">
-	<input type="hidden" name="PASSWORD" value="{$data['PASSWORD']}">
-	<input type="hidden" name="mode" value="NTIME">
-	<input type="hidden" name="NUMBER" value="{$suf}">
-	<input type="text" size="4" name="YEAR" value="{$time['tm_year']}">年
-	<input type="text" size="2" name="MON" value="{$time['tm_mon']}">月
-	<input type="text" size="2" name="DATE" value="{$time['tm_mday']}">日
-	<input type="text" size="2" name="HOUR" value="{$time['tm_hour']}">時
-	<input type="text" size="2" name="MIN" value="{$time['tm_min']}">分
-	<input type="text" size="2" name="NSEC" value="{$time['tm_sec']}">秒
-	<input type="submit" value="変更">
-</form>
-<form action="{$this->this_file}" method="post">
-	<input type="hidden" name="PASSWORD" value="{$data['PASSWORD']}">
-	<input type="hidden" name="mode" value="STIME">
-	<input type="hidden" name="NUMBER" value="{$suf}">
-	1970年1月1日から<input type="text" size="32" name="SSEC" value="$lastTime">秒
-	<input type="submit" value="秒指定で変更">
-</form>
-END;
-		}
+        if(!$this->is_backup) {
+            $this->time = localtime($this->lastTime, TRUE);
+            $this->time['tm_year'] += 1900;
+            $this->time['tm_mon']++;
+        }
     }
 
-    function backupPrint($data) {
-		global $init;
+    function readDatFile($file_path) {
+        $fp = fopen($file_path, "r");
 
-        $dir = opendir("./");
-		while($dn = readdir($dir)) {
-            $_dirName = preg_quote($init->dirName, "/");
+        $this->lastTurn = chop(fgets($fp, READ_LINE));
+        $this->lastTime = chop(fgets($fp, READ_LINE));
 
-			if(preg_match("/{$_dirName}\.bak(.*)$/", $dn, $suf)) {
-				if (is_file("{$init->dirName}.bak{$suf[1]}/hakojima.dat")) {
-					$this->dataPrint($data, $suf[1]);
-				}
-			}
-        }
-		closedir($dir);
+        fclose($fp);
     }
 
 }
--- app/views/admin/mente/main.php
+++ app/views/admin/mente/main.php
@@ -1,8 +1,46 @@
 <h1 class="title">メンテナンスツール</h1>
 
-<?php if (is_dir("{$init->dirName}")): ?>
+<?php if ($this->exist_log): ?>
     <hr>
-    <?php $this->dataPrint($data); ?>
+    <?php if (!$this->is_backup): ?>
+    <h2>現役データ</h2>
+    <?php else: ?>
+    <h2>バックアップ<?= $suf; ?></h2>
+    <?php endif; ?>
+
+    <strong>ターン<?= $this->lastTurn; ?></strong><br>
+    <strong>最終更新時間</strong>:<?= $this->timeString; ?><br>
+    <strong>最終更新時間(秒数表示)</strong>:1970年1月1日から<?= $this->lastTime ?> 秒<br>
+    <form action="<?= $this->this_file; ?>" method="post">
+        <input type="hidden" name="PASSWORD" value="<?= $data['PASSWORD']; ?>">
+        <input type="hidden" name="mode" value="DELETE">
+        <input type="hidden" name="NUMBER" value="<?= $suf; ?>">
+        <input type="submit" value="このデータを削除">
+    </form>
+
+    <?php if (!$this->is_backup): ?>
+    <h3>最終更新時間の変更</h3>
+    <form action="<?= $this->this_file ?>" method="post">
+        <input type="hidden" name="PASSWORD" value="<?= $data['PASSWORD'] ?>">
+        <input type="hidden" name="mode" value="NTIME">
+        <input type="hidden" name="NUMBER" value="<?= $suf ?>">
+        <input type="text" size="4" name="YEAR" value="<?= $this->time['tm_year'] ?>">年
+        <input type="text" size="2" name="MON" value="<?= $this->time['tm_mon'] ?>">月
+        <input type="text" size="2" name="DATE" value="<?= $this->time['tm_mday'] ?>">日
+        <input type="text" size="2" name="HOUR" value="<?= $this->time['tm_hour'] ?>">時
+        <input type="text" size="2" name="MIN" value="<?= $this->time['tm_min'] ?>">分
+        <input type="text" size="2" name="NSEC" value="<?= $this->time['tm_sec'] ?>">秒
+        <input type="submit" value="変更">
+    </form>
+    <form action="<?= $this->this_file ?>" method="post">
+        <input type="hidden" name="PASSWORD" value="<?= $data['PASSWORD'] ?>">
+        <input type="hidden" name="mode" value="STIME">
+        <input type="hidden" name="NUMBER" value="<?= $suf ?>">
+        1970年1月1日から<input type="text" size="32" name="SSEC" value="<?= $this->lastTime ?>">秒
+        <input type="submit" value="秒指定で変更">
+    </form>
+    <?php endif; ?>
+
 <?php else: ?>
     <hr>
     <form action="<?= $this->this_file; ?>" method="post">
