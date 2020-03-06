82617770ea3db1a4c84691eb4c043e51fa4e37d6
hiro0218 <hiro0218@gmail.com>
2020-03-07 02:40:17 +0900

    fix: meta tag

--- app/views/header.php
+++ app/views/header.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 	<title><?= $init->title ?></title>
 	<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
 	<link rel="stylesheet" type="text/css" href="<?= $init->cssDir ?>/<?= $init->css ?>">
