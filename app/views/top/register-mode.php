8556e36ad6a78638e390f7a06571f6a2455219d7
hiro0218 <hiro0218@gmail.com>
2020-03-08 02:20:13 +0900

    chore: メンバー変数を参照

--- app/views/top/register-mode.php
+++ app/views/top/register-mode.php
@@ -1,4 +1,4 @@
-<form action="<?= $this_file ?>?mode=conf" method="POST">
+<form action="<?= $this->this_file ?>?mode=conf" method="POST">
     <div class="text-right">
         <input TYPE="password" NAME="PASSWORD" SIZE=8 MAXLENGTH=32 required>
         <input TYPE="submit" VALUE="管理用" NAME="AdminButton">
