a0a16263440a9d4d5e24c554a4bd9d2b5d23c8f2
hiro0218 <hiro0218@gmail.com>
2020-03-09 00:17:28 +0900

    fix: format

--- app/controller/config.php
+++ app/controller/config.php
@@ -2,29 +2,31 @@
 
 namespace Controller;
 
-require_once PRESENTER_PATH.'/Config.php';
-require_once MODEL_PATH.'/File/Hako.php';
-require_once MODEL_PATH.'/Cgi.php';
+require_once PRESENTER_PATH . '/Config.php';
+require_once MODEL_PATH . '/File/Hako.php';
+require_once MODEL_PATH . '/Cgi.php';
 
-class Config {
-
-	function __construct() {
+class Config
+{
+    function __construct()
+    {
         $this->view();
     }
 
-    function view() {
-		$hako = new \Hako();
-		$cgi  = new \Cgi();
+    function view()
+    {
+        $hako = new \Hako();
+        $cgi = new \Cgi();
 
-		$cgi->parseInputData();
-		$cgi->getCookies();
+        $cgi->parseInputData();
+        $cgi->getCookies();
 
         $result = $hako->readIslands($cgi);
-		if(!$result) {
-			HTML::header();
-			ErrorHandler::noDataFile();
-			HTML::footer();
-			exit();
+        if (!$result) {
+            HTML::header();
+            ErrorHandler::noDataFile();
+            HTML::footer();
+            exit();
         }
 
         $html = new \Presenter\Config($hako, $cgi->dataSet);
--- app/presenter/Config.php
+++ app/presenter/Config.php
@@ -2,55 +2,62 @@
 
 namespace Presenter;
 
-require_once PRESENTER_PATH.'/HTML.php';
+require_once PRESENTER_PATH . '/HTML.php';
 
-class Config extends \HTML {
-
+class Config extends \HTML
+{
     private $data = [];
 
-    function __construct($hako, $dataSet) {
-		global $init;
+    function __construct($hako, $dataSet)
+    {
+        global $init;
 
         $this->init = $init;
-		$this->this_file = $init->baseDir . "/hako-main.php";
+        $this->this_file = $init->baseDir . "/hako-main.php";
         $this->hako = $hako;
         $this->islandList = $hako->islandList;
         $this->dataSet = $dataSet;
     }
 
-	public function setData($key, $value) {
-		$this->data[$key] = $value;
+    public function setData($key, $value)
+    {
+        $this->data[$key] = $value;
     }
 
-	public function getData($key) {
-		return $this->data[$key] ?? '';
+    public function getData($key)
+    {
+        return $this->data[$key] ?? '';
     }
 
-	/**
-	 * 島の登録と設定
-	 */
-	function renderPage() {
-		echo $this->render(VIEWS_PATH.'/conf/index.php');
-	}
+    /**
+     * 島の登録と設定
+     */
+    function renderPage()
+    {
+        echo $this->render(VIEWS_PATH . '/conf/index.php');
+    }
 
-	/**
-	 * 新しい島を探す
-	 */
-	function discovery() {
-		echo $this->render(VIEWS_PATH.'/conf/discovery.php');
-	}
+    /**
+     * 新しい島を探す
+     */
+    function discovery()
+    {
+        echo $this->render(VIEWS_PATH . '/conf/discovery.php');
+    }
 
-	/**
-	 * 島の名前とパスワードの変更
-	 */
-	function changeIslandInfo() {
-		echo $this->render(VIEWS_PATH.'/conf/change/island-info.php');
-	}
+    /**
+     * 島の名前とパスワードの変更
+     */
+    function changeIslandInfo()
+    {
+        echo $this->render(VIEWS_PATH . '/conf/change/island-info.php');
+    }
 
-	/**
-	 * オーナー名の変更
-	 */
-	function changeOwnerName() {
-		echo $this->render(VIEWS_PATH.'/conf/change/owner-name.php');
-	}
+    /**
+     * オーナー名の変更
+     */
+    function changeOwnerName()
+    {
+        echo $this->render(VIEWS_PATH . '/conf/change/owner-name.php');
+    }
 }
