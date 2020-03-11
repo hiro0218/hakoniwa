27dcc4edc2086cac0e42c1c4c410f233a012c0c5
hiro0218 <hiro0218@gmail.com>
2020-03-12 01:12:18 +0900

    fix: 定義を移動

--- app/views/category/index.php
+++ app/views/category/index.php
@@ -2,16 +2,12 @@
     <div class="table-responsive">
         <table class="table table-condensed">
 <?php
-		$element   = ['point', 'money', 'food', 'pop', 'area', 'fire', 'pots', 'gold', 'rice', 'peop', 'monster', 'taiji', 'farm', 'factory', 'commerce', 'hatuden', 'mountain', 'team'];
-		$bumonName = ["総合ポイント", $this->init->nameFunds, $this->init->nameFood, $this->init->namePopulation, $this->init->nameArea, "軍事力", "成長", "収入", "収穫", "人口増加", "怪獣出現数", "怪獣退治数", "農場", "工場", "商業", "発電所", "採掘場", "サッカー"];
-		$bumonUnit = ['pts', $this->init->unitMoney, $this->init->unitFood, $this->init->unitPop, $this->init->unitArea, "機密事項", "pts↑", $this->init->unitMoney, $this->init->unitFood, $this->init->unitPop, $this->init->unitMonster, $this->init->unitMonster, "0{$this->init->unitPop}", "0{$this->init->unitPop}", "0{$this->init->unitPop}", "000kw", "0{$this->init->unitPop}", 'pts'];
-
-		for($r = 0; $r < sizeof($element); $r++) {
+		for($r = 0; $r < sizeof($this->element); $r++) {
 			$max = 0;
 			for($i = 0; $i < $this->hako->islandNumber; $i++) {
 				$island = $this->hako->islands[$i];
-				if(($island[$element[$r]] > $max) && ($island['isBF'] != 1)) {
-					$max = $island[$element[$r]];
+				if(($island[$this->element[$r]] > $max) && ($island['isBF'] != 1)) {
+					$max = $island[$this->element[$r]];
 					$rankid[$r] = $i;
 				}
 			}
@@ -22,7 +18,7 @@
 				echo "<td width=\"15%\" class=\"M\">";
 
 				echo "<table class=\"table table-bordered\" style=\"border:0\">\n";
-				echo "<thead><tr><th {$this->init->bgTitleCell}>{$this->init->tagTH_}{$bumonName[$r]}{$this->init->_tagTH}</th></tr></thead>\n";
+				echo "<thead><tr><th {$this->init->bgTitleCell}>{$this->init->tagTH_}{$this->bumonName[$r]}{$this->init->_tagTH}</th></tr></thead>\n";
 				echo "<tr><td class=\"TenkiCell\">{$this->init->tagName_}-{$this->init->_tagName}</td></tr>\n";
 				echo "<tr><td class=\"TenkiCell\">-</td></tr>\n";
 				echo "</table>";
@@ -46,9 +42,9 @@
 				echo "<td width=\"15%\" class=\"M\">";
 
 				echo "<table class=\"table table-bordered\">\n";
-				echo "<thead><tr><th {$this->init->bgTitleCell}>{$this->init->tagTH_}{$bumonName[$r]}{$this->init->_tagTH}</th></tr></thead>\n";
+				echo "<thead><tr><th {$this->init->bgTitleCell}>{$this->init->tagTH_}{$this->bumonName[$r]}{$this->init->_tagTH}</th></tr></thead>\n";
 				echo "<tr><td class=\"TenkiCell\"><a href=\"{$this->this_file}?Sight={$island['id']}\">{$this->init->tagName_}{$name}{$this->init->_tagName}</a></td></tr>\n";
-				echo "<tr><td class=\"TenkiCell\">{$max}{$bumonUnit[$r]}</td></tr>\n";
+				echo "<tr><td class=\"TenkiCell\">{$max}{$this->bumonUnit[$r]}</td></tr>\n";
 				echo "</table>";
 				echo "</td>";
 
